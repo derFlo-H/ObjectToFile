24ec3338fa6f6bab6017f71c95bba8ae9ed52bdf
Florian <codingwithflorian@gmail.com>
2021-10-06 17:00:39 +0200

    Added FileIO file and comments

---
+++ src/objectToFile/Florian/FileIO.java
@@ -0,0 +1,12 @@
+package objectToFile.Florian;
+
+/*
+ * File Input and Output
+ * 
+ * Date 061021
+ * Author Florian
+ */
+
+public class FileIO {
+
+}
--- src/objectToFile/Florian/Main.java
+++ src/objectToFile/Florian/Main.java
@@ -1,5 +1,12 @@
 package objectToFile.Florian;
 
+/*
+ * A simple program that converts an Object to a file and back
+ * 
+ * Date 061021
+ * Author Florian
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
