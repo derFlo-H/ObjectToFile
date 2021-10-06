04c91a4428609b09678dfedbdc141b7c699b7ee1
Florian <codingwithflorian@gmail.com>
2021-10-06 16:27:37 +0200

    Created Main class and main JUnit file

---
+++ src/objectToFile/Florian/Main.java
@@ -0,0 +1,10 @@
+package objectToFile.Florian;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/objectToFile/Florian/TestMain.java
@@ -0,0 +1,14 @@
+package test.objectToFile.Florian;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestMain {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
