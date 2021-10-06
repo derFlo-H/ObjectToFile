187485c6c521fa84a947672f0636804b39ba31fa
Florian <codingwithflorian@gmail.com>
2021-10-06 17:11:26 +0200

    added JUnit tests

--- src/test/objectToFile/Florian/TestMain.java
+++ src/test/objectToFile/Florian/TestMain.java
@@ -4,11 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import objectToFile.Florian.FileIO;
+
 class TestMain {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testFileInput() {
+		assertTrue(FileIO.read("flo.txt") == "1234");
+		
+	}
+	
+	@Test
+	void testFileOutput() {
+		FileIO.write("len.txt","1234");
+		assertTrue(FileIO.read("len.txt") == "1234");
+		
 	}
 
 }
